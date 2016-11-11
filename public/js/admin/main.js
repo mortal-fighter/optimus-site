2141dbb94e99a01e5c36df678e0aca3f4bf17506
kirillvladykin <kirillmybox@rambler.ru>
2016-11-11 21:21:40 +0300

    - Commit before checkout

--- public/js/admin/main.js
+++ public/js/admin/main.js
@@ -1,18 +1,21 @@
-'use strict';
+(function(){
+	'use strict';
 
-//todo: взял с потолка, нужно точно вычислить
-const gap = 35;
+	//todo: взял с потолка, нужно точно вычислить
+	const gap = 35;
 
-function resizeColumns() {
-	$('.right-col').outerWidth($('.container-fluid').outerWidth() - $('.left-col').outerWidth() - gap);
-}
+	function resizeColumns() {
+		$('.right-col').outerWidth($('.container-fluid').outerWidth() - $('.left-col').outerWidth() - gap);
+	}
 
-function attachHandlers() {
-	// window resize
-	$(window).on('resize', resizeColumns);
-}
+	function attachHandlers() {
+		// window resize
+		$(window).on('resize', resizeColumns);
+	}
 
-$(document).ready(function(){
-	resizeColumns();
-	attachHandlers();
-});
+	$(document).ready(function(){
+		resizeColumns();
+		attachHandlers();
+	});	
+})()
+	
--- public/js/admin/news_create.js
+++ public/js/admin/news_create.js
@@ -1,9 +1,34 @@
-'use strict';
-
-$(document).function(function() {
-	var wbbOpt = {
-		lang: 'ru',
-		buttons: "bold,italic,underline"
+(function() {
+	'use strict';
+	
+	function initBBEditor() {
+		var wbbOpt = {
+			lang: 'ru',
+			buttons: 'bold,italic,underline'
+		}
+		$('#text-full').wysibb(wbbOpt);
 	}
-	$("#text-full").wysibb(wbbOpt);
-});
+
+	function attachHandlers() {
+		$('#news-create').on('click', function() {
+			$.ajax({
+				method: 'POST',
+				url: '/admin/news',
+				data: { 
+					title: encodeURIComponent($('#title').val()), 
+					textShort: encodeURIComponent($('#text-short').val()), 
+					textFull: encodeURIComponent($('#text-full').bbcode()) 
+				}
+			})
+		});
+		$('#news-cancel').on('click', function() {
+			//todo: is it always corrent return address
+			window.location.href='/admin/news';
+		});
+	}
+
+	$(document).ready(function() {
+		initBBEditor();
+		attachHandlers();
+	});	
+})()
