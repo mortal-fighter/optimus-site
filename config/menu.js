5b6bd721e55360ba704c543df62827c687a6895f
kirillvladykin <kirillmybox@rambler.ru>
2017-03-02 11:29:23 +0300

    - Добавил баннеры партнеров на главную страницу - Поправил стили

--- config/menu.js
+++ config/menu.js
@@ -44,13 +44,6 @@ module.exports = {
 			topMenu: true
 		},
 		{
-			titleShort: 'Интересное',
-			titleFull: 'Интересные видео о робототехнике',
-			titleCaption: 'Посмотри',
-			href: '/interesting',
-			topMenu: true
-		},
-		{
 			titleShort: 'Галерея',
 			titleFull: 'Галерея',
 			href: '/photos',
